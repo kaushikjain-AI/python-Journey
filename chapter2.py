0ea37666984f87d2fb5c4b91f80fb00a53f8fa7e
kaushikjain-AI <kaushikjain644@gmail.com>
2026-01-21 23:16:17 +0530

    Add files via upload

---
+++ chapter2.py
@@ -0,0 +1,86 @@
+# str1="this is a string. \t hii python"
+# str2='kauhsik'
+# str3="""this is a string"""
+
+# print(str1)
+
+# str1="hello"
+# len1=len(str1)
+# print(len1)
+# str2="world"
+# len2=len(str2)
+# print(len2)
+# finalstr=(str1+ " " +str2)
+# print(finalstr)
+# print(len(finalstr))
+
+# str="kaushik"
+# print(str[3])
+
+# str="kaushik"
+# print(str[:5])
+
+# str="i am kaushik"
+# print(str.endswith("ik"))
+
+# str="i am kaushik"
+# jk=str.capitalize()
+# print(jk)
+
+# #OR
+
+# str="i am"
+# print(str.capitalize())
+
+# str="i am kaushik"
+# print(str.replace("kaushik","paridhi"))
+
+# str="i am kaushik jain"
+# print(str.find("a"))
+
+# str1="kaushik"
+# print(len(str1))
+
+# age=19
+
+# if(age>=18):
+#     print("can vote")
+
+    #or
+
+# age=21
+
+# if(True):
+#     print("can vote")
+
+# light="pink"
+
+# if(light == "red"):
+#     print("stop")
+# elif(light == "yellow"):
+#     print("look")
+# elif(light == "green"):
+#     print("go") 
+# else:
+#     print("light is broken") 
+
+# age=1
+# if(age>=18):
+#     print("can vote")
+# else:
+#     print("cannot vote")   # ye space ko INTENTATION kehte h
+
+# marks=int(input("enter marks here:"))
+
+# if(marks>=90):
+#     grade="A"
+# elif(marks>=80 and marks<90):
+#     grade="B"
+# elif(marks>=70 and marks<=80):
+#     grade="C"
+# else:
+#     grade="D"
+
+# print("grade of the student:", grade)
+
+
